c30694c06de1055b1755eacf4200788605db82a8
Vorobaa <126954506+Vorobaa@users.noreply.github.com>
2024-05-11 21:26:45 +0300

    Додатки

--- board/__init__.py
+++ board/__init__.py
@@ -13,13 +13,25 @@
 
 
 from flask import Flask
-from board import pages, posts
+from board import pages, posts, database
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
+
+    database.init_app(app)
+    app.config.from_prefixed_env()
+
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
+
+    print(os.getenv("ENVIRONMENT"))
+    print(f"Using Database: {app.config.get('DATABASE')}")
+
     return app
 
 
